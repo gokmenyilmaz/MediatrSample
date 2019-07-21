88231658371d132c0b223b4aefe19c512796d036
GökmenYilmaz <gokmen1977@msn.com>
2019-07-21 22:54:20 +0300

    rr

---
+++ SlnMediatrSample/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class HomeController : ControllerBase
+    {
+        private readonly IMediator _mediatr;
+
+        public HomeController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+        [HttpGet("")]
+        public ActionResult<string> NotifyAll()
+        {
+            _mediatr.Publish(new NotificationMessage { NotifyText = "metin" });
+            return "Completed";
+        }
+    }
+}
---
+++ SlnMediatrSample/Mediatr1.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class NotificationMessage : INotification
+    {
+        public string NotifyText { get; set; }
+    }
+
+    public class Notifier1 : INotificationHandler<NotificationMessage>
+    {
+        public Task Handle(NotificationMessage notification, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine($"Debugging from Notifier 1. Message  : {notification.NotifyText} ");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Notifier2 : INotificationHandler<NotificationMessage>
+    {
+        public Task Handle(NotificationMessage notification, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine($"Debugging from Notifier 2. Message  : {notification.NotifyText} ");
+            return Task.CompletedTask;
+        }
+    }
+
+  
+}
